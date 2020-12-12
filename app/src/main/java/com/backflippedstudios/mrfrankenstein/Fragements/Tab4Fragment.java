f673be8313294decae1bb2627286fe22dd2ccbcd
Adam Corbin <acorbin3@gmail.com>
2020-12-12 04:45:57 -0500

    Updating gradle files

--- app/src/main/java/com/backflippedstudios/mrfrankenstein/Fragements/Tab4Fragment.java
+++ app/src/main/java/com/backflippedstudios/mrfrankenstein/Fragements/Tab4Fragment.java
@@ -12,7 +12,7 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
